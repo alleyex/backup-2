db4224be647d349d904809fa4d771a20af76072b
alleyex <alleyex@hotmail.com>
2018-10-06 13:11:35 +0800

    move position via srcoll

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, ElementRef } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-home',
@@ -7,15 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent {
     categorys = [
-        { title: '天氣路況', isActive: false  },
-        { title: '公告訊息', isActive: false },
-        { title: '便民服務', isActive: false },
-        { title: '產品服務', isActive: false }
-    ];
+        { title: '天氣路況', id: 'status' },
+        { title: '公告訊息', id: 'notice' },
+        { title: '便民服務', id: 'service' },
+        { title: '產品服務', id: 'production' }
+    ];   
 
-    onClick($element):void{
-        console.log($element);
-        //$element.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+    getOffsetTop(element: any) {
+        let offsetTop = element.offsetTop || 0;
+        if (element.offsetParent) {
+            offsetTop += this.getOffsetTop(element.offsetParent);
+        }
+        return offsetTop;
+    }
+
+    onClick($element): void {        
+        window.scrollTo(0,this.getOffsetTop($element));
+    }
+
+    
+    moveScroll(category:any): void {  
+        let element = document.getElementById(category.id);
+        window.scrollTo(0,this.getOffsetTop(element));         
     }
 
 
